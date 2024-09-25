f410d0dd7cab9c7297a8a5f6ebd4f81a17a992be
Noel Dupuis <noel.dupuis45370@gmail.com>
2024-09-25 15:51:06 +0200

    Register Adapters, initiate repository and provide it to the widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:panorama/feature/accounts/accounts_page.dart';
+import 'package:panorama/feature/accounts/model/account.dart';
+import 'package:panorama/feature/accounts/repositories/account_repository.dart';
+import 'package:panorama/feature/accounts/repositories/hive_account_repository.dart';
 import 'package:panorama/feature/core/app_bar.dart';
 import 'package:panorama/feature/core/body.dart';
 import 'package:panorama/feature/core/bottom_navigation_bar.dart';
@@ -17,77 +20,95 @@ void main() async {
   final appDocumentDir = await getApplicationDocumentsDirectory();
   Hive.init(appDocumentDir.path);
 
+  // Register Adapters
+  Hive.registerAdapter(AccountAdapter());
+  Hive.registerAdapter(AccountTypeAdapter());
+
+  // Open Hive box
+  final accountBox = await Hive.openBox<Account>('accountsBox');
+
+  // Create the repository
+  final accountRepository = HiveAccountRepository(accountBox);
+
   Bloc.observer = SimpleBlocObserver();
 
-  runApp(const MainApp());
+  runApp(MainApp(accountRepository: accountRepository));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final HiveAccountRepository accountRepository;
+  const MainApp({super.key, required this.accountRepository});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: FlexThemeData.light(
-        scheme: FlexScheme.cyanM3,
-        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-        blendLevel: 7,
-        subThemesData: const FlexSubThemesData(
-          blendOnLevel: 10,
-          blendOnColors: false,
-          useTextTheme: true,
-          useM2StyleDividerInM3: true,
-          alignedDropdown: true,
-          useInputDecoratorThemeInDialogs: true,
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider(
+          create: (context) => accountRepository,
         ),
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-        useMaterial3: true,
-        swapLegacyOnMaterial3: true,
-      ),
-      darkTheme: FlexThemeData.dark(
-        scheme: FlexScheme.cyanM3,
-        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-        blendLevel: 13,
-        subThemesData: const FlexSubThemesData(
-          blendOnLevel: 20,
-          useTextTheme: true,
-          useM2StyleDividerInM3: true,
-          alignedDropdown: true,
-          useInputDecoratorThemeInDialogs: true,
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: FlexThemeData.light(
+          scheme: FlexScheme.cyanM3,
+          surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+          blendLevel: 7,
+          subThemesData: const FlexSubThemesData(
+            blendOnLevel: 10,
+            blendOnColors: false,
+            useTextTheme: true,
+            useM2StyleDividerInM3: true,
+            alignedDropdown: true,
+            useInputDecoratorThemeInDialogs: true,
+          ),
+          visualDensity: FlexColorScheme.comfortablePlatformDensity,
+          useMaterial3: true,
+          swapLegacyOnMaterial3: true,
         ),
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-        useMaterial3: true,
-        swapLegacyOnMaterial3: true,
-      ),
-      themeMode: ThemeMode.light,
-      home: BlocProvider(
-        create: (context) => AppNavigationBloc(),
-        child: BlocBuilder<AppNavigationBloc, AppNavigationState>(
-          builder: (context, state) {
-            switch (state.currentPage) {
-              case AppPageEnum.accounts:
-                return const AccountsPage();
-              case AppPageEnum.categories:
-                return Scaffold(
-                  appBar: AppBar(
-                    title: PanoramaAppBarTitle(),
-                  ),
-                  body: PanoramaBody(),
-                  floatingActionButton: PanoramaFloatingActionButton(),
-                  bottomNavigationBar: const PanoramaBottomNavBar(),
-                );
-              case AppPageEnum.transactions:
-                return Scaffold(
-                  appBar: AppBar(
-                    title: PanoramaAppBarTitle(),
-                  ),
-                  body: PanoramaBody(),
-                  floatingActionButton: PanoramaFloatingActionButton(),
-                  bottomNavigationBar: const PanoramaBottomNavBar(),
-                );
-            }
-          },
+        darkTheme: FlexThemeData.dark(
+          scheme: FlexScheme.cyanM3,
+          surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+          blendLevel: 13,
+          subThemesData: const FlexSubThemesData(
+            blendOnLevel: 20,
+            useTextTheme: true,
+            useM2StyleDividerInM3: true,
+            alignedDropdown: true,
+            useInputDecoratorThemeInDialogs: true,
+          ),
+          visualDensity: FlexColorScheme.comfortablePlatformDensity,
+          useMaterial3: true,
+          swapLegacyOnMaterial3: true,
+        ),
+        themeMode: ThemeMode.light,
+        home: BlocProvider(
+          create: (context) => AppNavigationBloc(),
+          child: BlocBuilder<AppNavigationBloc, AppNavigationState>(
+            builder: (context, state) {
+              switch (state.currentPage) {
+                case AppPageEnum.accounts:
+                  return const AccountsPage();
+                case AppPageEnum.categories:
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: PanoramaAppBarTitle(),
+                    ),
+                    body: PanoramaBody(),
+                    floatingActionButton: PanoramaFloatingActionButton(),
+                    bottomNavigationBar: const PanoramaBottomNavBar(),
+                  );
+                case AppPageEnum.transactions:
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: PanoramaAppBarTitle(),
+                    ),
+                    body: PanoramaBody(),
+                    floatingActionButton: PanoramaFloatingActionButton(),
+                    bottomNavigationBar: const PanoramaBottomNavBar(),
+                  );
+              }
+            },
+          ),
         ),
       ),
     );
